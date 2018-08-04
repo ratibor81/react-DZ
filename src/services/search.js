b187bbcf3efb4f751680aab971a0b241832e84d3
unknown <smrserg2017@gmail.com>
2018-08-04 12:42:34 +0300

    Fixed search-button

--- src/services/search.js
+++ src/services/search.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const API_KEY = '532f680f186ee3009db06b2e2efe9aab';
 const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
 
-const searchMovie = ({ title, onSuccess, onError }) => {
-  const url = `${BASE_URL}?api_key=${API_KEY}&language=en-EN&query=${title}&page=1`;
+const searchMovie = ({ title, onSuccess, onError, pageNum }) => {
+  const url = `${BASE_URL}?api_key=${API_KEY}&language=en-EN&query=${title}&page=${pageNum}`;
 
   return axios
     .get(url)
