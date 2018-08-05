5f5017b096e5d5da0c4bddf29c987a3d11bd7224
unknown <smrserg2017@gmail.com>
2018-08-05 12:04:41 +0300

    Add animations

--- src/components/watch-list/index.jsx
+++ src/components/watch-list/index.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import styles from './styles.css';
 import WatchListCard from '../watch-list-card';
 
 const WatchList = ({ watchlist, removeCard, toggleModal }) => (
   <div className={styles.list}>
     <h2 className={styles.header}>Watchlist</h2>
-    <ul className={styles.movie_list}>
+    <TransitionGroup component="ul">
       {watchlist.map(movie => (
-        <li className={styles.card} key={movie.id}>
-          <WatchListCard
-            {...movie}
-            removeCard={removeCard}
-            toggleModal={toggleModal}
-          />
-        </li>
+        <CSSTransition
+          key={movie.id}
+          timeout={200}
+          classNames={{
+            enter: styles.slideEnter,
+            enterActive: styles.slideEnterActive,
+            exit: styles.slideExit,
+            exitActive: styles.slideExitActive,
+          }}
+          mountOnEnter
+        >
+          <li className={styles.card} key={movie.id}>
+            <WatchListCard
+              {...movie}
+              removeCard={removeCard}
+              toggleModal={toggleModal}
+            />
+          </li>
+        </CSSTransition>
       ))}
-    </ul>
+    </TransitionGroup>
   </div>
 );
 
