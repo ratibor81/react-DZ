a936097003dea649c268632751af5e314482f7b4
unknown <smrserg2017@gmail.com>
2018-09-10 13:14:20 +0300

    Refactoring Routes and Routes-render

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
-// import { compose } from 'redux';
 import { hot } from 'react-hot-loader';
-// import { connect } from 'react-redux';
 import { Switch } from 'react-router';
 import { Route } from 'react-router-dom';
 // import { AnimatedSwitch, AnimatedRoute } from 'react-router-transition';
 // import PropTypes from 'prop-types';
-import * as routes from '@constants/routes';
+import ROUTES from '@constants/routes';
 import withAuthentication from '@hoc/withAuthentication';
 import Navigation from '../navigation';
 // import styles from './styles.css';
-import {
-  SignInPage,
-  SignUpPage,
-  AccountPage,
-  PasswordForgetPage,
-  MovieInfoPage,
-  WatchListPage,
-  NotFoundPage,
-  MoviesPage,
-} from '../async-pages';
 
 class App extends Component {
   componentDidMount() {
@@ -38,22 +26,14 @@ class App extends Component {
         <Navigation />
 
         <Switch>
-          <Route exact path={routes.HOME} component={MoviesPage} />
-          <Route
-            exact
-            path={`${routes.MOVIES}/:movieId`}
-            component={MovieInfoPage}
-          />
-          <Route exact path={routes.SIGN_UP} component={SignUpPage} />
-          <Route exact path={routes.SIGN_IN} component={SignInPage} />
-          <Route
-            exact
-            path={routes.PASSWORD_FORGET}
-            component={PasswordForgetPage}
-          />
-          <Route exact path={routes.WATCHLIST} component={WatchListPage} />
-          <Route exact path={routes.ACCOUNT} component={AccountPage} />
-          <Route component={NotFoundPage} />
+          {ROUTES.map((route, index) => (
+            <Route
+              key={String(index)}
+              path={route.path}
+              exact={route.exact}
+              component={route.component}
+            />
+          ))}
         </Switch>
       </div>
     );
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,3 +1,6 @@
+import Loadable from 'react-loadable';
+import Loader from '@shared/loader';
+
 export const HOME = '/';
 export const SIGN_IN = '/signin';
 export const SIGN_UP = '/signup';
@@ -5,3 +8,78 @@ export const ACCOUNT = '/account';
 export const PASSWORD_FORGET = '/pw-forget';
 export const WATCHLIST = '/watchlist';
 export const MOVIES = '/movies';
+
+export default [
+  {
+    path: HOME,
+    exact: true,
+    component: Loadable({
+      loader: () =>
+        import('@pages/movies' /* webpackChunkName: "movies-page" */),
+      loading: Loader,
+    }),
+  },
+  {
+    path: SIGN_IN,
+    exact: true,
+    component: Loadable({
+      loader: () =>
+        import('@pages/auth-manager/SignInPage' /* webpackChunkName: "signin-page" */),
+      loading: Loader,
+    }),
+  },
+  {
+    path: SIGN_UP,
+    exact: true,
+    component: Loadable({
+      loader: () =>
+        import('@pages/auth-manager/SignUpPage' /* webpackChunkName: "signup-page" */),
+      loading: Loader,
+    }),
+  },
+  {
+    path: ACCOUNT,
+    exact: true,
+    component: Loadable({
+      loader: () =>
+        import('@pages/auth-manager/AccountPage' /* webpackChunkName: "account-page" */),
+      loading: Loader,
+    }),
+  },
+  {
+    path: PASSWORD_FORGET,
+    exact: true,
+    component: Loadable({
+      loader: () =>
+        import('@pages/auth-manager/PasswordForgetPage' /* webpackChunkName: "passwordforget-page" */),
+      loading: Loader,
+    }),
+  },
+  {
+    path: `${MOVIES}/:movieId`,
+    exact: true,
+    component: Loadable({
+      loader: () =>
+        import('@pages/movie-info' /* webpackChunkName: "movieinfo-page" */),
+      loading: Loader,
+    }),
+  },
+  {
+    path: WATCHLIST,
+    exact: true,
+    component: Loadable({
+      loader: () =>
+        import('@pages/watchlist' /* webpackChunkName: "watchlist-page" */),
+      loading: Loader,
+    }),
+  },
+  {
+    path: null,
+    exact: false,
+    component: Loadable({
+      loader: () =>
+        import('@pages/not-found-page' /* webpackChunkName: "not-found-page" */),
+      loading: Loader,
+    }),
+  },
+];
