c084c395db1b96e8a0ad65389cabbf374e0196a3
unknown <smrserg2017@gmail.com>
2018-09-10 19:55:43 +0300

    Added <Portal> to Snackbar

--- src/components/shared-ui/snackBar/index.jsx
+++ src/components/shared-ui/snackBar/index.jsx
@@ -5,6 +5,7 @@ import SnackbarContent from '@material-ui/core/SnackbarContent';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import green from '@material-ui/core/colors/green';
 import { withStyles } from '@material-ui/core/styles';
+import Portal from '@material-ui/core/Portal';
 
 const styles = theme => ({
   snackbar: {
@@ -25,26 +26,28 @@ const styles = theme => ({
 });
 
 const SnackBar = ({ text, open, close, classes }) => (
-  <Snackbar
-    className={classes.snackbar}
-    anchorOrigin={{
-      vertical: 'top',
-      horizontal: 'center',
-    }}
-    open={open}
-    onClose={close}
-    autoHideDuration={1000}
-  >
-    <SnackbarContent
-      className={classes.success}
-      message={
-        <div className={classes.message}>
-          <CheckCircleIcon className={classes.icon} />
-          {text}
-        </div>
-      }
-    />
-  </Snackbar>
+  <Portal>
+    <Snackbar
+      className={classes.snackbar}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+      open={open}
+      onClose={close}
+      autoHideDuration={1000}
+    >
+      <SnackbarContent
+        className={classes.success}
+        message={
+          <div className={classes.message}>
+            <CheckCircleIcon className={classes.icon} />
+            {text}
+          </div>
+        }
+      />
+    </Snackbar>
+  </Portal>
 );
 
 SnackBar.propTypes = {
